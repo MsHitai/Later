51e641cc30f081c9807dc34eb1de4c03bc6b39ca
MsHitai <mmalafeeva@yandex.ru>
2023-08-14 14:41:08 +0300

    feat: подключить Boot

--- src/main/java/ru/practicum/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/item/repository/ItemRepository.java
@@ -1,14 +1,13 @@
 package ru.practicum.item.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 import ru.practicum.item.model.Item;
 
 import java.util.List;
 
 @Repository
-public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
+public interface ItemRepository extends JpaRepository<Item, Long> {
 
     List<Item> findByUserId(long userId);
 
